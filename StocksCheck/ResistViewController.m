9655cffebce21024f2f7164746d3b16eb02b7704
0gravity000 <0gravity000@gmail.com>
2016-03-05 17:52:20 +0900

    Commit 0009
    Edit ResistViewController
    add Code Search process

--- StocksCheck/ResistViewController.m
+++ StocksCheck/ResistViewController.m
@@ -43,6 +43,10 @@
     self.stocksArray = [NSMutableArray array];
     self.searchResultArray = [NSMutableArray array];
     [self.stocksArray removeAllObjects];
+    
+    [self loadImageFromRemote];
+    NSLog(@"load from remote");
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,7 +54,131 @@
     // Dispose of any resources that can be recreated.
 }
 
+// HTTP からファイルをロード
+- (void)loadImageFromRemote
+{
+    NSFileManager* sharedFM = [NSFileManager defaultManager];
+    NSArray* possibleURLs = [sharedFM URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSURL* documentsDir = nil;
+    NSURL* documentsNameWithDir = nil;
+    NSString *fileName = @"stocks.txt";
+    NSError *error = nil;
+    //NSURL *sorceUrl = [NSURL URLWithString:@"http://0gravity000.web.fc2.com/xxx_stockList/stocks.txt"];
+    BOOL IsSuccess;
+    
+    if ([possibleURLs count] >= 1) {
+        // Use the first directory (if multiple are returned)
+        documentsDir = [possibleURLs objectAtIndex:0];
+        documentsNameWithDir = [documentsDir URLByAppendingPathComponent:fileName];
+        //delete file
+        IsSuccess = [sharedFM removeItemAtURL:documentsNameWithDir error:&error];
+        if (IsSuccess) {
+            NSLog(@"success detele");
+        } else {
+            NSLog(@"fail detele");
+            NSLog(@"Error !: %@", [error localizedDescription]);
+        }
+    }
+    // 読み込むファイルの URL を作成
+    NSURL *url = [NSURL URLWithString:@"http://0gravity000.web.fc2.com/xxx_stockList/stocks.txt"];
+    
+    NSString *strData = [[NSString alloc] initWithContentsOfURL:url  encoding:NSUTF16StringEncoding error:&error];
+    NSLog(@"Error !: %@", [error localizedDescription]);
+    
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *dataPath = [documentsDirectory stringByAppendingPathComponent:@"stocks.txt"];
+    
+    //NSString *data;
+    BOOL success = [fileManager fileExistsAtPath:dataPath];
+    if (success) {
+        strData = [NSString stringWithContentsOfFile:dataPath encoding:NSUTF16StringEncoding error:nil];
+    } else {
+        [strData writeToFile:dataPath atomically:YES encoding:NSUTF16StringEncoding error:&error];
+    }
+    NSLog(@"Error !: %@", [error localizedDescription]);
+    
+    [self readStocksTextdata:strData];
+    
+}
+
+-(void) readStocksTextdata:(NSString *)data {
+    
+    // UTF16 エンコードされた CSV ファイル
+    //    NSString *filePath = @"/Users/rakuishi/Desktop/test.csv";
+    //    NSString *text = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    // 改行文字で区切って配列に格納する
+    //NSArray *lines = [text componentsSeparatedByString:@"\n"];
+    NSArray *lines = [data componentsSeparatedByString:@"\n"];
+    NSLog(@"lines count: %ld", lines.count);    // 行数
+    
+    NSString *key;
+    NSString *value;
+    
+    for (NSString *row in lines) {
+        // コンマで区切って配列に格納する
+        NSArray *items = [row componentsSeparatedByString:@","];
+        NSMutableDictionary *stocksDic = [NSMutableDictionary dictionary];
+        
+        int cnt = 1;
+        for (NSString *column in items) {
+            //[self.keyArray addObject:[@"key" stringByAppendingString:[NSString stringWithFormat:@"%d", cnt]]];
+            //[self.valueArray addObject:column];
+            key = [@"key" stringByAppendingString:[NSString stringWithFormat:@"%d", cnt]];
+            value = column;
+            [stocksDic setObject:value forKey:key];
+            cnt++;
+        }
+        [self.stocksArray addObject:stocksDic];
+        //NSLog(@"%@", items);778
+    }
+    //先頭の2行はヘッダーデータで不要なので削除
+    [self.stocksArray removeObjectsInRange:NSMakeRange(0, 2)];
+    NSLog(@"self.stocksArray count: %ld", lines.count);    // 行数
+    
+    //---reload table view
+    //[self.resistTableView reloadData];
+}
+
+
 #pragma mark - UISearchBarDelegate
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    // called when text changes (including clear)
+    [self.searchResultArray removeAllObjects];
+    NSString *searchStr = self.codeSearchBar.text;
+    
+    if (![searchStr isEqualToString:@""]) {
+        //search in array
+        int i=0;
+        BOOL bHitFlag;
+        for (i=0; i < [self.stocksArray count]; i++) {
+            bHitFlag = false;
+            NSDictionary *dic = [self.stocksArray objectAtIndex:i];
+            NSString *code = [dic objectForKey:@"key1"];
+            NSString *name = [dic objectForKey:@"key3"];
+            
+            NSRange codeRange = [code rangeOfString:searchStr];
+            if (codeRange.location != NSNotFound) {
+                bHitFlag = true;
+            }
+            NSRange nameRange = [name rangeOfString:searchStr];
+            if (nameRange.location != NSNotFound) {
+                bHitFlag = true;
+            }
+            
+            if (bHitFlag == true) {
+                [self.searchResultArray addObject:dic];
+            }
+        }
+    }
+    //---reload table view
+    [self.resistTableView reloadData];
+    
+}
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
     // called when keyboard search button pressed
@@ -59,6 +187,15 @@
     //close View
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    // called when text starts editing
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
 
 #pragma mark - Table View
 
@@ -94,12 +231,12 @@
 {
     //if ([self.stocksArray count] > 0) {
     if ([self.searchResultArray count] > 0) {
-//        NSDictionary *dic;
-//        dic = [self.searchResultArray objectAtIndex:indexPath.row];
-//        //Code
-//        cell.textLabel.text = [dic objectForKey:@"key1"];
-//        //銘柄名
-//        cell.detailTextLabel.text = [dic objectForKey:@"key3"];
+        NSDictionary *dic;
+        dic = [self.searchResultArray objectAtIndex:indexPath.row];
+        //Code
+        cell.textLabel.text = [dic objectForKey:@"key1"];
+        //銘柄名
+        cell.detailTextLabel.text = [dic objectForKey:@"key3"];
         
     }
 }
