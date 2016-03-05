bade2977cd198b3e9be00b95bc749822abd940cd
0gravity000 <0gravity000@gmail.com>
2016-03-05 12:42:29 +0900

    Initial Commit

---
+++ StocksCheck/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  StocksCheck
+//
+//  Created by SASAKIAI on 2016/03/05.
+//  Copyright © 2016年 SASAKIAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
